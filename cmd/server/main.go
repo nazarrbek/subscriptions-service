a77eeb35cb9d22d1bf15d29247bce7fa1f9e76e4
nazarbek <241355@astanait.edu.kz>
2026-06-27 14:32:24 +0500

    Initialize HTTP server with Chi router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/", checkResponse)
+	log.Println("Server started on :8080")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func checkResponse(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("Hello world"))
+	if err != nil {
+		log.Printf("failed to write response: %v", err)
+		return
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/nazarrbek/subscriptions-service
 
 go 1.26.3
+
+require (
+	github.com/go-chi/chi/v5 v5.3.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.10.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/text v0.29.0 // indirect
+)
